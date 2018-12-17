fe2b94782a3cc59be0c01d4f3d0e252e043e6bea
Joey Lovato <joeylovato1@gmail.com>
2018-12-17 00:47:28 -0700

    Added View Controllers classes for all views; added test launch screen, added some icons

---
+++ bluesky/ViewControllers/AutoIDRequestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AutoIDRequestViewController.swift
+//  bluesky
+//
+//  Created by Joey Lovato on 12/17/18.
+//  Copyright © 2018 Blue Sky. All rights reserved.
+//
+
+import UIKit
+
+class AutoIDRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ bluesky/ViewControllers/CertRequestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CertRequestViewController.swift
+//  bluesky
+//
+//  Created by Joey Lovato on 12/17/18.
+//  Copyright © 2018 Blue Sky. All rights reserved.
+//
+
+import UIKit
+
+class CertRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
